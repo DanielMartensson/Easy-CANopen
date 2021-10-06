420371e4043cfe9afe62502228b8f8a0aa91284d
danielmartensson <daniel.martensson100@outlook.com>
2021-10-06 22:30:38 +0200

    First update

---
+++ ReadMe.md
@@ -0,0 +1,12 @@
+# Easy CANopen
+
+This library use the protocol `CANopen`. It's a protocol made for industrial automation and it's told to be very complex. That's why I created
+this repository because I want `CANopen` to be easy to use.
+
+# How to use `CANopen`
+
+# Structure of this project
+
+# Questions and answers
+
+# Issues and answers
---
+++ src/main.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Easy.c
+ Author      : Daniel Mårtensson
+ Version     :
+ Copyright   : MIT
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Easy CANopen"); /* prints Easy CANopen */
+	return EXIT_SUCCESS;
+}
