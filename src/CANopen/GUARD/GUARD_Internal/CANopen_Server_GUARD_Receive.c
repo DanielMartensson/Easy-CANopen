dd9ef1f76fc3e7d977ad59b4d95bc17b336a3d64
danielmartensson <daniel.martensson100@outlook.com>
2021-11-07 01:24:41 +0100

    > to ==

--- src/CANopen/GUARD/GUARD_Internal/CANopen_Server_GUARD_Receive.c
+++ src/CANopen/GUARD/GUARD_Internal/CANopen_Server_GUARD_Receive.c
@@ -12,7 +12,7 @@
 
 void CANopen_Server_GUARD_Receive_Request_Status(CANopen *canopen, uint8_t node_ID, uint8_t data[]){
 	/* Check if guard is enabled */
-	if(canopen->od_communication.consumer_heartbeat_time > 0)
+	if(canopen->od_communication.consumer_heartbeat_time == 0)
 		return;
 
 	/* A guard request have zero data */
