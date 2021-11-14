b887004a3217a912f2ead1f1d93d67c495abbd8c
danielmartensson <daniel.martensson100@outlook.com>
2021-11-15 00:27:11 +0100

    Added PDO request

--- src/CANopen/PDO/PDO_Internal/PDO_Internal.h
+++ src/CANopen/PDO/PDO_Internal/PDO_Internal.h
@@ -12,6 +12,7 @@
 
 /* Internal software functions that the user don't need to call */
 void CANopen_Producer_PDO_Transmit_Data(CANopen *canopen);
+void CANopen_Producer_PDO_Receive_Request(CANopen *canopen, uint8_t node_ID, uint8_t data[]);
 void CANopen_Consumer_PDO_Receive_Data(CANopen *canopen, uint8_t node_ID, uint8_t data[]);
 
 
